f9cc947bdb71f1579176f4a2a88c7854a8cb277e
Colin <colin.diesh@gmail.com>
2017-02-10 14:13:56 -0500

    Optimize rendering

--- js/View/FeatureGlyph/MAF.js
+++ js/View/FeatureGlyph/MAF.js
@@ -19,7 +19,6 @@ function (
             var block = fRect.viewInfo.block;
             var scale = fRect.viewInfo.scale;
             var charSize = this.getCharacterMeasurements( context );
-            var thisB = this;
             var s = feature.get('start');
             var h = this.config.style.height;
             var vals = feature.get('alignments');
@@ -30,50 +29,53 @@ function (
             }
             
 
-            this.config.samples.forEach(function (key) {
+            for(var j = 0; j < this.config.samples.length; j++) {
+                var key = this.config.samples[j];
                 if (vals[key]) {
-                    var pos = thisB.config.samples.indexOf(key);
+                    var pos = j;
                     var alignment = vals[key].data;
-                    context.fillStyle = thisB.config.style.gapColor;
+
+                    var left = fRect.viewInfo.block.bpToX(s);
+                    var right = fRect.viewInfo.block.bpToX(s + 1);
+                    var delta = right - left;
+
+                    //gaps
+                    context.fillStyle = this.config.style.gapColor;
                     for (var i = 0; i < alignment.length; i++) {
-                        var left = fRect.viewInfo.block.bpToX(s + i);
-                        var right = fRect.viewInfo.block.bpToX(s + i + 1);
+                        var l = left + delta*i;
                         if (alignment[i] === '-') {
-                            context.fillRect(left, 3 / 8 * h + h * pos, right - left + 0.6, h / 4);
+                            context.fillRect(l, 3 / 8 * h + h * pos, delta + 0.6, h / 4);
                         }
                     }
+                    //mismatches
+                    context.fillStyle = this.config.style.mismatchColor;
                     for (var i = 0; i < alignment.length; i++) {
-                        var left = fRect.viewInfo.block.bpToX(s + i);
-                        var right = fRect.viewInfo.block.bpToX(s + i + 1);
-                        if (alignment[i] !== '-') {
-                            if (seq[i].toLowerCase() !== alignment[i].toLowerCase()) {
-                                context.fillStyle = thisB.config.style.mismatchColor;
-                                context.fillRect(left, 1 / 4 * h + h * pos, right - left + 0.6, h / 2);
-
-                                if( right-left >= charSize.w) {
-                                    context.font = thisB.config.style.mismatchFont;
-
-                                    context.fillStyle = 'white';
-                                    var offset = ((right-left)-charSize.w)/2+
-                                    context.fillText( alignment[i], left+offset, h/2+h*pos+2, right-left + 0.6, h/2)
-                                }
-                            } else {
-                                context.fillStyle = thisB.config.style.matchColor;
-                                context.fillRect(left, 1 / 4 * h + h * pos, right - left + 0.6, h / 2);
-
-                                
-                            }
-                            if( right-left >= charSize.w) {
-                                context.font = thisB.config.style.mismatchFont;
-
-                                context.fillStyle = 'white';
-                                var offset = ((right-left)-charSize.w)/2+1
-                                context.fillText( alignment[i], left+offset, h/2+h*pos+2, right-left + 0.6, h/2)
-                            }
+                        var l = left + delta*i;
+                        if (seq[i].toLowerCase() !== alignment[i].toLowerCase() && alignment[i] !== '-') {
+                            context.fillRect(l, 1 / 4 * h + h * pos, delta + 0.6, h / 2);
+                        }
+                    }
+                    //matches
+                    context.fillStyle = this.config.style.matchColor;
+                    for (var i = 0; i < alignment.length; i++) {
+                        var l = left + delta*i;
+                        if (seq[i].toLowerCase() === alignment[i].toLowerCase()) {
+                            context.fillRect(l, 1 / 4 * h + h * pos, delta + 0.6, h / 2);
+                        }
+                        
+                    }
+                    //font
+                    context.font = this.config.style.mismatchFont;
+                    context.fillStyle = 'white';
+                    for (var i = 0; i < alignment.length; i++) {
+                        var l = left + delta*i;
+                        if( delta >= charSize.w) {
+                            var offset = (delta-charSize.w)/2+1
+                            context.fillText( alignment[i], l+offset, h/2+h*pos+2, delta + 0.6, h/2)
                         }
                     }
                 }
-            });
+            }
             return 0;
         },
         _defaultConfig: function() {
